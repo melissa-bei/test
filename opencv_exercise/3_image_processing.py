6e031c286b59822f984fcbdeaa2daf3fc9bf1a3a
libei <libei@cadg.cn>
2020-10-23 10:53:47 +0800

    完成轮廓检测的作业，学习轮廓的层次结构

--- opencv_exercise/3_image_processing.py
+++ opencv_exercise/3_image_processing.py
@@ -793,49 +793,114 @@ plt.rcParams['axes.unicode_minus'] = False
 # print(cv.matchShapes(cnt_a, cnt_c, 1, 0.0))                          # a和c的形状差异度
 
 
-# Homework1：Check the documentation for cv.pointPolygonTest(), you can find a nice image in Red and Blue color. It
-# represents the distance from all pixels to the white curve on it. All pixels inside curve is blue depending on the
-# distance. Similarly outside points are red. Contour edges are marked with White. So problem is simple. Write a code
-# to create such a representation of distance.
+# # Homework1：Check the documentation for cv.pointPolygonTest(), you can find a nice image in Red and Blue color. It
+# # represents the distance from all pixels to the white curve on it. All pixels inside curve is blue depending on the
+# # distance. Similarly outside points are red. Contour edges are marked with White. So problem is simple. Write a code
+# # to create such a representation of distance.
+# img = cv.imread("hand.png")
+# rows, cols, _ = img.shape
+# ret, thresh = cv.threshold(cv.cvtColor(img, cv.COLOR_BGR2GRAY), 127, 255, cv.THRESH_BINARY_INV)
+# contours, hierarchy = cv.findContours(thresh, 1, 2)
+# cnt = contours[0]
+#
+# for r in range(rows):
+#     for c in range(cols):
+#         dst = cv.pointPolygonTest(cnt,                               # 轮廓
+#                                   (c, r),                            # (x, y)
+#                                   True)                              # 返回具体距离
+#         level = max(200 - abs(dst), 0)
+#         if dst > 0:                                                  # 在轮廓内，设置为红色
+#             img[r, c, :] = [0, 0, level]
+#         elif dst == 0:
+#             img[r, c, :] = [255, 255, 255]
+#         else:
+#             img[r, c, :] = [level, 0, 0]
+# cv.imshow("color dist", img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
 
 
-# Homework2：Compare images of digits or letters using cv.matchShapes(). ( That would be a simple step towards OCR )
-from tensorflow.keras.datasets import mnist                          # 从mnist库获取手写数字
-(x, y), (_, _) = mnist.load_data("mnist.npz")                        # 导入MNIST数据接口
-size = 50
-x = np.asarray([cv.resize(x[i, :, :], (128, 128)) for i in range(size)])
-y = y[:size]
-cnts = []
-
-plt_x = 10
-plt_y = int(np.ceil(size / plt_x))
-for idx in range(size):
-    cnt = cv.findContours(x[idx, :, :], 1, 2)[0]                     # 计算所有数字的轮廓
-    cnts.append(cnt)
-    plt.subplot(plt_y, plt_x, idx + 1)
-    plt.imshow(x[idx, :, :], cmap="gray")
-    plt.title(y[idx]), plt.xticks([]), plt.yticks([])
-plt.show()
-
-cnts_1 = []
-for idx in range(size):
-    if y[idx] == 1:                                                  # 找出所有数字1的轮廓
-        cnts_1.append(cnts[idx])
-        img = x[idx, :, :]
-        # img = np.expand_dims(img, axis=0)
-        # img = np.concatenate((img, img, img), axis=0)
-        # tmp = cnts_1[-1]
-        cv.drawContours(img, cnts_1[-1], -1, (0, 255, 0), 2)
-        cv.imshow("num", img
-                  )
-        cv.waitKey(0)
-        cv.destroyWindow("num")
-
-for i in range(len(cnts_1)):                                         # 计算每两个1之间的差异值
-    for j in range(i + 1, len(cnts_1)):
-        val = cv.matchShapes(cnts_1[i][0], cnts_1[j][0], 1, 0.0)
-        print(val)
+# # Homework2：Compare images of digits or letters using cv.matchShapes(). ( That would be a simple step towards OCR )
+# from tensorflow.keras.datasets import mnist                          # 从mnist库获取手写数字
+# (x, y), (_, _) = mnist.load_data("mnist.npz")                        # 导入MNIST数据接口
+# size = 50
+# x = np.asarray([cv.resize(x[i, :, :], (128, 128)) for i in range(size)])
+# y = y[:size]
+# cnts = []
+#
+# plt_x = 10
+# plt_y = int(np.ceil(size / plt_x))
+# for idx in range(size):
+#     cnt = cv.findContours(x[idx, :, :], 1, 2)[0]                     # 计算所有数字的轮廓
+#     cnts.append(cnt)
+#     plt.subplot(plt_y, plt_x, idx + 1)
+#     plt.imshow(x[idx, :, :], cmap="gray")
+#     plt.title(y[idx]), plt.xticks([]), plt.yticks([])
+# plt.show()
+#
+# cnts_1 = []
+# for idx in range(size):
+#     if y[idx] == 1:                                                  # 找出所有数字1的轮廓
+#         cnts_1.append(cnts[idx])
+#         img = x[idx, :, :]
+#         cv.drawContours(img, cnts_1[-1], -1, (255, 255, 255), 2)
+#         cv.imshow("num", img)
+#         cv.waitKey(0)
+#         cv.destroyWindow("num")
+#
+# for i in range(len(cnts_1)):                                         # 计算每两个1之间的差异值
+#     for j in range(i + 1, len(cnts_1)):
+#         val = cv.matchShapes(cnts_1[i][0], cnts_1[j][0], 1, 0.0)
+#         print(i + 1, j + 1, val)
 
 
+# 轮廓层次----------------------------------------------------------------------------------------------------------------
+"""使用cv.findContours（）函数在图像中找到轮廓时，传递了一个参数--轮廓检索模式。通常取cv.RETR_LIST或cv.RETR_TREE，效果很好。
+hierarchy----[相同等级下一个轮廓的编号，    没有时为-1
+              相同等级上一个轮廓编号，      没有时为-1
+              第一个子轮廓，              没有时为-1
+              父轮廓]                   没有时为-1
+cv.RETR_LIST：                  不创建父子关系，所有轮廓均平等，即hierarchy的后两项始终为-1
+cv.RETR_TREE：                  创建轮廓树
+cv.RETR_CCOMP：                 生成二级轮廓，外圈为第一层级，内权威第二层级，由最内层二级轮廓及外，包含父轮廓和子轮廓
+cv.RETR_EXTERNAL：              仅返回最外层轮廓，也就没有父轮廓和子轮廓，只有兄弟轮廓
+0的编号有疑问
+"""
+img = 255 - cv.imread("contours.png")
+ret, thresh = cv.threshold(cv.cvtColor(img, cv.COLOR_BGR2GRAY), 127, 255, cv.THRESH_BINARY)
 
+contours, hierarchy = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
+print("list:", hierarchy)
+# cv.drawContours(img, contours, -1, (0, 0, 255), 3)
+# cv.imshow("c", img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
 
+contours, hierarchy = cv.findContours(thresh,
+                                      cv.RETR_EXTERNAL,
+                                      cv.CHAIN_APPROX_SIMPLE)
+print("父轮廓:", hierarchy)
+# cv.drawContours(img, contours, -1, (0, 0, 255), 3)
+# cv.imshow("c", img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+img = 255 - cv.imread("contours2.png")
+ret, thresh = cv.threshold(cv.cvtColor(img, cv.COLOR_BGR2GRAY), 127, 255, cv.THRESH_BINARY)
+contours, hierarchy = cv.findContours(thresh,
+                                      cv.RETR_CCOMP,
+                                      cv.CHAIN_APPROX_SIMPLE)
+print("二级轮廓:", hierarchy)
+# cv.drawContours(img, contours, -1, (0, 0, 255), 3)
+# cv.imshow("c", img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+contours, hierarchy = cv.findContours(thresh,
+                                      cv.RETR_TREE,
+                                      cv.CHAIN_APPROX_SIMPLE)
+print("轮廓树:", hierarchy)
+# cv.drawContours(img, contours, -1, (0, 0, 255), 3)
+# cv.imshow("c", img)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
