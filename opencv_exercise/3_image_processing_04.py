06cd6f403e134e4d957835ee549d72279954644f
libei <libei@cadg.cn>
2020-10-23 17:46:28 +0800

    学习傅里叶变换（1）

--- opencv_exercise/3_image_processing_04.py
+++ opencv_exercise/3_image_processing_04.py
@@ -53,8 +53,8 @@ plt.rcParams['axes.unicode_minus'] = False
 # plt.subplot(224), plt.plot(hist_full), plt.plot(hist_mask)
 # plt.xlim([0, 256])
 # plt.show()
-
-
+#
+#
 # # 直方图均衡--------------------------------------------------------------------------------------------------------------
 # img = cv.imread("foggy.png", 0)
 #
@@ -107,42 +107,89 @@ plt.rcParams['axes.unicode_minus'] = False
 #                                        [[0, 180], [0, 256]])         # [x的范围, y的范围]
 # plt.imshow(hist, interpolation="nearest")
 # plt.show()
+#
+#
+# # 直方图反投影????????????????????????????????????????????????TODO:没有明白具体原理
+# target = cv.imread("rock.png")
+# hsvt = cv.cvtColor(target, cv.COLOR_BGR2HSV)                              # 目标图像hsv
+#
+# roi = target[:100, :300, :]
+# hsv = cv.cvtColor(roi, cv.COLOR_BGR2HSV)                                  # roi hsv
+#
+# roihist = cv.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])  # roi hist
+#
+# # # numpy方法
+# # targethist = cv.calcHist([hsvt], [0, 1], None, [180, 256], [0, 180, 0, 256])
+# # R = roihist / targethist
+# # h, s, v = cv.split(hsv)
+# # dst = R[h.ravel(), s.ravel()]
+# # dst = np.minimum(dst, 1)
+# # dst = dst.reshape(hsvt.shape[:2])
+#
+# # opencv方法，返回与目标直方图相同的概率图像
+# cv.normalize(roihist, roihist, 0, 255, cv.NORM_MINMAX)                    # 归一化roi hist
+# dst = cv.calcBackProject([hsvt],
+#                          [0, 1],
+#                          roihist,                                         # 传入的直方图需进行标准化
+#                          [0, 180, 0, 256],
+#                          1)
+#
+# disc = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))                 # 获取核
+# cv.filter2D(dst, -1, disc, dst)
+#
+# ret, thresh = cv.threshold(dst, 127, 255, 0)                              # 期望图像的阈值范围
+# thresh = cv.merge((thresh, thresh, thresh))
+# res = cv.bitwise_and(target, thresh)
+#
+# cv.imshow("figure", np.vstack((target, thresh, res)))
+# cv.waitKey(0)
+# cv.destroyAllWindows()
 
 
-# 直方图反投影????????????????????????????????????????????????TODO:没有明白具体原理
-target = cv.imread("rock.png")
-hsvt = cv.cvtColor(target, cv.COLOR_BGR2HSV)                              # 目标图像hsv
+# 图像转换----------------------------------------------------------------------------------------------------------------
+"""cv.dft(), cv.idft()傅里叶变换和傅里叶逆变换"""
+img = cv.imread("messi.png", 0)
+fft = np.fft.fft2(img)                                                 # numpy中傅里叶变换
+# np.fft.fft2默认返回结果零频率分量是位于左上角要使其居中则要偏移一半可以用np.fft.fftshift()来实现
+fftshift = np.fft.fftshift(fft)
+magnitude_spectrum = 20 * np.log(np.abs(fftshift))                     # 中间最亮，说明低频多
 
-roi = target[:100, :300, :]
-hsv = cv.cvtColor(roi, cv.COLOR_BGR2HSV)                                  # roi hsv
+plt.subplot(121), plt.imshow(img, cmap='gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
 
-roihist = cv.calcHist([hsv], [0, 1], None, [180, 256], [0, 180, 0, 256])  # roi hist
+rows, cols = img.shape
+crow, ccol = rows // 2, cols // 2
+fftshift[crow - 30:crow + 31, ccol - 30:ccol + 31] = 0               # 使用一个60*60的矩形窗口过滤低频
+fft_ishift = np.fft.ifftshift(fftshift)                              # 使用np.fft.ifftshift()把频谱图平移回去
+img_back = np.fft.ifft2(fft_ishift)                                  # 使用np.ifft2()找到逆FFT
+img_back = np.real(img_back)                                         # 取复数的实部
 
-# # numpy方法
-# targethist = cv.calcHist([hsvt], [0, 1], None, [180, 256], [0, 180, 0, 256])
-# R = roihist / targethist
-# h, s, v = cv.split(hsv)
-# dst = R[h.ravel(), s.ravel()]
-# dst = np.minimum(dst, 1)
-# dst = dst.reshape(hsvt.shape[:2])
+plt.subplot(121), plt.imshow(img, cmap='gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(img_back, cmap='gray')                  # 高通滤波是是边缘检测操作
+plt.title('Image after HPF'), plt.xticks([]), plt.yticks([])
+plt.show()
 
-# opencv方法，返回与目标直方图相同的概率图像
-cv.normalize(roihist, roihist, 0, 255, cv.NORM_MINMAX)                    # 归一化roi hist
-dst = cv.calcBackProject([hsvt],
-                         [0, 1],
-                         roihist,                                         # 传入的直方图需进行标准化
-                         [0, 180, 0, 256],
-                         1)
+"""但可以看出有一些振铃效果，这是应为使用矩形窗口遮盖引起的，矩形被转换为了正弦波形，所以一般不采用矩形窗口滤波，而是采用高斯窗口。
+OpenCV为此提供了功能cv.dft（）和cv.idft（）。返回与以前相同的结果，但是有两个通道。第一个通道将具有结果的实部，第二个通道将具有结果的虚部。
+输入的图像应首先转换为np.float32。
+"""
+dft = cv.dft(np.float32(img), flags=cv.DFT_COMPLEX_OUTPUT)
+dft_shift = np.fft.fftshift(dft)
 
-disc = cv.getStructuringElement(cv.MORPH_ELLIPSE, (5, 5))                 # 获取核
-cv.filter2D(dst, -1, disc, dst)
+itude_spectrum = 20 * np.log(cv.magnitude(dft_shift[:, :, 0],        # np.fft.fftshift(fft)效果相同
+                                          dft_shift[:, :, 1]))
+# 还可以使用cv.cartToPolar（）一次返回大小和相位
 
-ret, thresh = cv.threshold(dst, 127, 255, 0)                              # 期望图像的阈值范围
-thresh = cv.merge((thresh, thresh, thresh))
-res = cv.bitwise_and(target, thresh)
+plt.subplot(121), plt.imshow(img, cmap='gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122), plt.imshow(magnitude_spectrum, cmap='gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
 
-cv.imshow("figure", np.vstack((target, thresh, res)))
-cv.waitKey(0)
-cv.destroyAllWindows()
+
 
 
